790d1408995a31c5e993ed9b9250af47daa340d2
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2022-04-24 04:09:31 +0700

    Update index.js

--- messaging-app-with-CRA/src/pages/Login/index.js
+++ messaging-app-with-CRA/src/pages/Login/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./index.css";
 
-import { Amity } from "../../components/Amity";
-
 export function Login({ onConnect = () => {} }) {
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -16,8 +14,8 @@ export function Login({ onConnect = () => {} }) {
       <div className="Modal">
    
         
-        <img src="https://www.img.in.th/images/3946e9ecd20a3c502414e4db7f7e3a56.png" width="40%" />
-        <h1>Connect with chat - THANA Business.</h1>
+        <img src="https://www.img.in.th/images/3946e9ecd20a3c502414e4db7f7e3a56.png" width="50%" />
+        <h3>Connect Chat with your</h3>
           
         <form onSubmit={onSubmit}>
           <input

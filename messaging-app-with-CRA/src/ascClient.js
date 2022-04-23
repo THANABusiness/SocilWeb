909409d5c4788f4b0e8ef6d3831b5160b4e49967
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2022-04-24 04:21:43 +0700

    Update ascClient.js

--- messaging-app-with-CRA/src/ascClient.js
+++ messaging-app-with-CRA/src/ascClient.js
@@ -1,8 +1,8 @@
 import ASCClient, { ConnectionStatus, ApiEndpoint } from "@amityco/js-sdk";
 
 // Amity sample-app apiKey
-const apiKey = "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78";
-export const client = ASCClient.create({ apiKey, apiEndpoint: ApiEndpoint.SG });
+const apiKey = "b0eeee5e3fd8f76d1932851f5709168884008ee0e8353b7f";
+export const client = ASCClient.create({ apiKey, apiEndpoint: ApiEndpoint.US });
 
 export let currentUserId = null;
 

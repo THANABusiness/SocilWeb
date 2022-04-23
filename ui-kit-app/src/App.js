84e66b9d9d788b8a87ca8a4e633136d48958aef7
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2022-04-24 03:19:27 +0700

    Update App.js

--- ui-kit-app/src/App.js
+++ ui-kit-app/src/App.js
@@ -5,7 +5,7 @@ import { AmityUiKitProvider, AmityUiKitSocial } from "@amityco/ui-kit";
 
 import Login from "./Login";
 
-const apiKey = "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78";
+const apiKey = "b0eeee5e3fd8f76d1932851f5709168884008ee0e8353b7f";
 
 export default function App() {
   const [userId, setUserId] = useState();
